6c5a2d06b2c9aa73150f194f713f4cb8456d87a1
Beata12 <beata.marek.94@gmail.com>
2025-03-28 18:15:15 +0100

    edu changes

--- js/contact_form.js
+++ js/contact_form.js
@@ -5,32 +5,32 @@ const emailInput = document.querySelector("#user_email");
 const messageInput = document.querySelector("#message");
 
 const publicKey = "e57EhNdvqqwWkpB9j";
-const serviceId = "service_zt2weso";
+const serviceId = "service_5qcxiiw";
 const templateId = "template_dlrbyup";
 
 emailjs.init(publicKey);
 
 contactForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	submitBtn.innerText = "Just a moment...";
-	const inputFields = {
-		name: nameInput.value,
-		email: emailInput.value,
-		message: messageInput.value,
-	};
-	emailjs
-		.send(serviceId, templateId, inputFields)
-		.then(() => {
-			submitBtn.innerText = "Message sent successfully";
-			nameInput.value = "";
-			emailInput.value = "";
-			messageInput.value = "";
-			setTimeout(() => {
-				submitBtn.innerText = "Send message";
-			}, 3000);
-		})
-		.catch((error) => {
-			console.error("Something went wrong:", error);
-			submitBtn.innerText = "Something went wrong";
-		});
+  e.preventDefault();
+  submitBtn.innerText = "Just a moment...";
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  emailjs
+    .send(serviceId, templateId, inputFields)
+    .then(() => {
+      submitBtn.innerText = "Message sent successfully";
+      nameInput.value = "";
+      emailInput.value = "";
+      messageInput.value = "";
+      setTimeout(() => {
+        submitBtn.innerText = "Send message";
+      }, 3000);
+    })
+    .catch((error) => {
+      console.error("Something went wrong:", error);
+      submitBtn.innerText = "Something went wrong";
+    });
 });
